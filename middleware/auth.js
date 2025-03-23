120da404b43167d170469f59d079e77c5e3a9b49
jimmy <jimmy20031111@gmail.com>
2025-03-23 12:43:48 +0800

    feat: 新增 JWT Middleware

---
+++ middleware/auth.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({
+      error: {
+        message: '未提供認證憑證'
+      }
+    });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({
+        error: {
+          message: '無效的憑證'
+        }
+      });
+    }
+    req.user = user;
+    next();
+  });
+};
+
+module.exports = authenticateToken;
